ac06a73758ad44b52421d4dbf738dfb084eb1831
rodrigues-i <lucas-f250@live.com>
2022-06-11 13:01:50 -0300

    Relacionamento entre metodo abstrato com metodo default e estatico na interface funcional

--- exercicios/lambdas/Calculo.java
+++ exercicios/lambdas/Calculo.java
@@ -1,6 +1,19 @@
 package lambdas;
 
+// o @FunctionalInterface estabelece que a interface deverá ter somente um metodo/funcao
+
+@FunctionalInterface
 public interface Calculo {
     
     public abstract double executar(double a, double b);
+
+    // ter um metodo default não impactará a interface funcional
+    default String legal() {
+        return "Legal";
+    }
+
+    // ter um metodo estático não afetará a interface funcional
+    static String muitoLegal() {
+        return "Muito legal";
+    }
 }
