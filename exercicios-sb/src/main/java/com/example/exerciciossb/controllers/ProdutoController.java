4e49a2f7b8e6ca34c8995a42b2031b6fe65edc53
rodrigues-i <lucas-f250@live.com>
2022-07-13 14:58:49 -0300

    Implement the get request to get all products and another one to get a single product by its id

--- exercicios-sb/src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -1,16 +1,19 @@
 package com.example.exerciciossb.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import com.example.exerciciossb.model.entities.Produto;
 import com.example.exerciciossb.model.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -23,5 +26,15 @@ public class ProdutoController {
 	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
+	}
+
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+
+	@GetMapping(path="/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
 	}
 }
